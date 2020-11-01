82db71856410561e7a1584c2838cb63134d703e3
anthr76 <hello@anthonyrabbito.com>
2020-11-01 08:40:08 -0500

    Profile update

--- terraform/typhoon/terraform/clusters/bare-metal/profiles.tf
+++ terraform/typhoon/terraform/clusters/bare-metal/profiles.tf
@@ -1,17 +1,17 @@
-locals {
-  # flatcar-stable -> stable channel
-  channel = split("-", var.os_channel)[1]
-}
+# locals {
+#   # flatcar-stable -> stable channel
+#   channel = split("-", var.os_channel)[1]
+# }
 
 // Flatcar Linux install profile (from release.flatcar-linux.net)
 resource "matchbox_profile" "flatcar-install" {
   count = length(var.controllers) + length(var.workers)
   name  = format("%s-flatcar-install-%s", var.cluster_name, concat(var.controllers.*.name, var.workers.*.name)[count.index])
-
-  kernel = "${var.download_protocol}://${local.channel}.release.flatcar-linux.net/amd64-usr/${var.os_version}/flatcar_production_pxe.vmlinuz"
+  
+  kernel = "${var.download_protocol}://${split("-", concat(var.controllers.*.os_channel, var.workers.*.os_channel)[count.index])[1]}.release.flatcar-linux.net/amd64-usr/${concat(var.controllers.*.os_version, var.workers.*.os_version)[count.index]}/flatcar_production_pxe.vmlinuz"
 
   initrd = [
-    "${var.download_protocol}://${local.channel}.release.flatcar-linux.net/amd64-usr/${var.os_version}/flatcar_production_pxe_image.cpio.gz",
+    "${var.download_protocol}://${split("-", concat(var.controllers.*.os_channel, var.workers.*.os_channel)[count.index])[1]}.release.flatcar-linux.net/amd64-usr/${concat(var.controllers.*.os_version, var.workers.*.os_version)[count.index]}/flatcar_production_pxe_image.cpio.gz",
   ]
 
   args = flatten([
@@ -32,10 +32,10 @@ resource "matchbox_profile" "cached-flatcar-install" {
   count = length(var.controllers) + length(var.workers)
   name  = format("%s-cached-flatcar-linux-install-%s", var.cluster_name, concat(var.controllers.*.name, var.workers.*.name)[count.index])
 
-  kernel = "/assets/flatcar/${var.os_version}/flatcar_production_pxe.vmlinuz"
+  kernel = "/assets/flatcar/${concat(var.controllers.*.os_version, var.workers.*.os_version)[count.index]}/flatcar_production_pxe.vmlinuz"
 
   initrd = [
-    "/assets/flatcar/${var.os_version}/flatcar_production_pxe_image.cpio.gz",
+    "/assets/flatcar/${concat(var.controllers.*.os_version, var.workers.*.os_version)[count.index]}/flatcar_production_pxe_image.cpio.gz",
   ]
 
   args = flatten([
@@ -56,8 +56,8 @@ data "template_file" "install-configs" {
   template = file("${path.module}/cl/install.yaml")
 
   vars = {
-    os_channel         = local.channel
-    os_version         = var.os_version
+    os_channel         = split("-", concat(var.controllers.*.os_channel, var.workers.*.os_channel)[count.index])[1]
+    os_version         = concat(var.controllers.*.os_version, var.workers.*.os_version)[count.index]
     ignition_endpoint  = format("%s/ignition", var.matchbox_http_endpoint)
     install_disk       = var.install_disk
     ssh_authorized_key = var.ssh_authorized_key
@@ -72,8 +72,8 @@ data "template_file" "cached-install-configs" {
   template = file("${path.module}/cl/install.yaml")
 
   vars = {
-    os_channel         = local.channel
-    os_version         = var.os_version
+    os_channel         = split("-", concat(var.controllers.*.os_channel, var.workers.*.os_channel)[count.index])[1]
+    os_version         = concat(var.controllers.*.os_version, var.workers.*.os_version)[count.index]
     ignition_endpoint  = format("%s/ignition", var.matchbox_http_endpoint)
     install_disk       = var.install_disk
     ssh_authorized_key = var.ssh_authorized_key
@@ -106,7 +106,7 @@ data "template_file" "controller-configs" {
     domain_name            = var.controllers.*.domain[count.index]
     etcd_name              = var.controllers.*.name[count.index]
     etcd_initial_cluster   = join(",", formatlist("%s=https://%s:2380", var.controllers.*.name, var.controllers.*.domain))
-    cgroup_driver          = var.os_channel == "flatcar-edge" ? "systemd" : "cgroupfs"
+    cgroup_driver          = concat(var.controllers.*.os_channel, var.workers.*.os_channel)[count.index] == "flatcar-edge" ? "systemd" : "cgroupfs"
     cluster_dns_service_ip = module.bootstrap.cluster_dns_service_ip
     cluster_domain_suffix  = var.cluster_domain_suffix
     ssh_authorized_key     = var.ssh_authorized_key
@@ -134,7 +134,7 @@ data "template_file" "worker-configs" {
 
   vars = {
     domain_name            = var.workers.*.domain[count.index]
-    cgroup_driver          = var.os_channel == "flatcar-edge" ? "systemd" : "cgroupfs"
+    cgroup_driver          = concat(var.controllers.*.os_channel, var.workers.*.os_channel)[count.index] == "flatcar-edge" ? "systemd" : "cgroupfs"
     cluster_dns_service_ip = module.bootstrap.cluster_dns_service_ip
     cluster_domain_suffix  = var.cluster_domain_suffix
     ssh_authorized_key     = var.ssh_authorized_key
