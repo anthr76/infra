b6094e808cccfadd0daf898de0d77e5e24e108d0
anthr76 <hello@anthonyrabbito.com>
2020-11-01 21:44:24 -0500

    Moved typhoon to my own repo and began infra directory to change from gitops to infra

--- terraform/infra/clusters/kvm/providers.tf
+++ terraform/infra/clusters/kvm/providers.tf
@@ -6,3 +6,16 @@ provider "matchbox" {
 }
 
 provider "ct" {}
+
+terraform {
+  required_providers {
+    ct = {
+      source  = "poseidon/ct"
+      version = "0.6.1"
+    }
+    matchbox = {
+      source = "poseidon/matchbox"
+      version = "0.4.1"
+    }
+  }
+}
