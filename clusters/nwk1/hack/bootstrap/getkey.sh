09fa4180ba22c4e6d65e352befec372a6230c5fc
anthr76 <hello@anthonyrabbito.com>
2020-11-20 08:11:05 -0500

    Correct pathing, but really need to work on this.
    Signed-off-by: anthr76 <hello@anthonyrabbito.com>

--- clusters/nwk1/hack/bootstrap/getkey.sh
+++ clusters/nwk1/hack/bootstrap/getkey.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-kubeseal --fetch-cert --controller-name=sealed-secrets --controller-namespace=kube-system > ../secrets/pub-cert.pem
+kubeseal --fetch-cert --controller-name=sealed-secrets --controller-namespace=kube-system > hack/secrets/pub-cert.pem
