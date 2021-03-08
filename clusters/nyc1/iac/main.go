c1474590fbb01fe1c4a2dc9fbfd55c837b1e27f7
anthr76 <hello@anthonyrabbito.com>
2021-03-07 21:05:40 -0500

    Started a loop
    Signed-off-by: anthr76 <hello@anthonyrabbito.com>

--- clusters/nyc1/iac/main.go
+++ clusters/nyc1/iac/main.go
@@ -7,9 +7,24 @@ import (
 	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
 )
 
-// Zypper is not installing packages at the moment. Maybe due to initrd networking bug. Either way the repo is added and user is added so Ansible can take care for now.
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
+
+		// Set number of nodes for cluster
+		masterNodes := 3
+		// workerNodes := 2
+
+		// Create a base UserData with Kubic's cloudinit
+		var cloudinit = pulumi.String(`#!/bin/bash
+		mount /var
+		mount /home
+		useradd -m localanthony
+		echo "localanthony ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/localanthony
+		mkdir -pm700 /home/localanthony/.ssh
+		cp /root/.ssh/authorized_keys /home/localanthony/.ssh/authorized_keys
+		chown localanthony:users -R /home/localanthony/.ssh`)
+
+		// Pull latest Kubic Image to use with DigitalOcean in NYC1
 		kubic, err := digitalocean.NewCustomImage(ctx, "kubic", &digitalocean.CustomImageArgs{
 			Url: pulumi.String("https://download.opensuse.org/tumbleweed/appliances/openSUSE-MicroOS.x86_64-Kubic-kubeadm-OpenStack-Cloud.qcow2"),
 			Regions: pulumi.StringArray{
@@ -19,28 +34,26 @@ func main() {
 		if err != nil {
 			return err
 		}
-		droplet, err := digitalocean.NewDroplet(ctx, "kubicTest", &digitalocean.DropletArgs{
-			Image:  kubic.ID(),
-			Region: pulumi.String("nyc1"),
-			Size:   pulumi.String("s-1vcpu-1gb"),
-			SshKeys: pulumi.StringArray{
-				pulumi.String("28165998"),
-			},
-			UserData: pulumi.String(`#!/bin/bash
-			mount /var
-			mount /home
-			useradd -m localanthony
-			echo "localanthony ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/localanthony
-			mkdir -pm700 /home/localanthony/.ssh
-			cp /root/.ssh/authorized_keys /home/localanthony/.ssh/authorized_keys
-			chown localanthony:users -R /home/localanthony/.ssh`),
-		})
 
-		if err != nil {
-			fmt.Printf("Error! %v", err)
-			return err
+		i := 1
+		for i <= masterNodes {
+			droplet, err := digitalocean.NewDroplet(ctx, "kubicMasterNodes", &digitalocean.DropletArgs{
+				Image:  kubic.ID(),
+				Region: pulumi.String("nyc1"),
+				Size:   pulumi.String("s-1vcpu-1gb"),
+				SshKeys: pulumi.StringArray{
+					pulumi.String("28165998"),
+				},
+				UserData: cloudinit,
+			})
+			if err != nil {
+				fmt.Printf("Error! %v", err)
+				return err
+			}
+			ctx.Export("Ipv4Address", droplet.Ipv4Address)
+			i++
+			return nil
 		}
-		ctx.Export("Ipv4Address", droplet.Ipv4Address)
 		return nil
 	})
 }
