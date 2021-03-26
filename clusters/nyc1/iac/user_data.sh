cb93849b367073fd3fd1dc5e1c9c4dbf8a647ff3
anthr76 <hello@anthonyrabbito.com>
2021-03-25 21:36:08 -0400

    Planning Cleanup
    Use internal-dns on dhcp at droplet creation.
    
    Include missing pushed handler
    
    Try addon phases for thin nodes.
    
    Signed-off-by: anthr76 <hello@anthonyrabbito.com>

--- clusters/nyc1/iac/user_data.sh
+++ clusters/nyc1/iac/user_data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cp /etc/sysconfig/network/ifcfg-eth0 /etc/sysconfig/network/ifcfg-eth1
 wicked ifup eth1
+echo 'NETCONFIG_DNS_STATIC_SERVERS="10.136.153.234 10.136.42.191"' >> /etc/sysconfig/network/config
+echo 'NETCONFIG_DNS_STATIC_SEARCHLIST="k8s.rabbito.tech"' >> /etc/sysconfig/network/config
+netconfig update -f
 useradd -m localanthony
 echo "localanthony ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/localanthony
 mkdir -pm700 /home/localanthony/.ssh
