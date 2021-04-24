b2f885f2f02a8b6d26b02deddefebde0360d6ac5
anthr76 <hello@anthonyrabbito.com>
2021-04-24 17:57:35 -0400

    Drop web on control-plane LB
    Signed-off-by: anthr76 <hello@anthonyrabbito.com>

--- clusters/nyc1/iac/firewall.tf
+++ clusters/nyc1/iac/firewall.tf
@@ -3,7 +3,7 @@ data "digitalocean_vpc" "nyc1_idm" {
 }
 
 resource "digitalocean_firewall" "k8s-cluster" {
-  name = "kubic-k8s"
+  name = "kubic-controlplane-k8s"
   droplet_ids = concat(digitalocean_droplet.kubic_worker.*.id, digitalocean_droplet.kubic_master.*.id, digitalocean_droplet.kubic_first_master.*.id)
 
   inbound_rule {
@@ -11,19 +11,6 @@ resource "digitalocean_firewall" "k8s-cluster" {
     port_range       = "6443"
     source_load_balancer_uids = [digitalocean_loadbalancer.kubic_k8s.id]
   }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "80"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
-  inbound_rule {
-    protocol         = "tcp"
-    port_range       = "443"
-    source_addresses = ["0.0.0.0/0", "::/0"]
-  }
-
   inbound_rule {
     protocol         = "icmp"
     source_addresses = ["0.0.0.0/0", "::/0"]
