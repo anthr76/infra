2a130e28fab9f83e23dda6be79c3af662b8902c6
Anthony Rabbito <ted437@gmail.com>
2021-06-06 21:37:07 +0000

    Declare Cloudflare DNS as code

---
+++ armature/prod/cloud-dns/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "http" {
+    address        = "https://gitlab.com/api/v4/projects/27033486/terraform/state/cloud-dns-prod"
+    lock_address   = "https://gitlab.com/api/v4/projects/27033486/terraform/state/cloud-dns-prod/lock"
+    unlock_address = "https://gitlab.com/api/v4/projects/27033486/terraform/state/cloud-dns-prod/lock"
+    lock_method    = "POST"
+    unlock_method  = "DELETE"
+    retry_wait_min = 5
+  }
+  required_providers {
+    sops = {
+      source  = "carlpett/sops"
+      version = "0.6.2"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "2.21.0"
+    }
+  }
+}
+
+provider "sops" {}
+
+data "sops_file" "tf_secrets" {
+  source_file = "tf-secrets.sops.yaml"
+}
+
+provider "cloudflare" {
+  email   = data.sops_file.tf_secrets.data["cf_email"]
+  api_key = data.sops_file.tf_secrets.data["cf_api_key"]
+}
