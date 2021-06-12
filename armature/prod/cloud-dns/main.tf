4af6aed543955b2e8f5673776ea0207a01bec862
Anthony Rabbito <ted437@gmail.com>
2021-06-12 02:47:13 +0000

    Update Terraform sops to v0.6.3

--- armature/prod/cloud-dns/main.tf
+++ armature/prod/cloud-dns/main.tf
@@ -10,7 +10,7 @@ terraform {
   required_providers {
     sops = {
       source  = "carlpett/sops"
-      version = "0.6.2"
+      version = "0.6.3"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
