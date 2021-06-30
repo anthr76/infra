d86489897662131110967c3e14ff0dd0a52f6ab0
anthr76 <hello@anthonyrabbito.com>
2021-06-30 13:33:25 -0400

    fix: typo
    Signed-off-by: anthr76 <hello@anthonyrabbito.com>

--- clusters/nwk1/iac/profile-amd64.tf
+++ clusters/nwk1/iac/profile-amd64.tf
@@ -24,7 +24,7 @@ resource "matchbox_group" "default_amd64" {
 
 resource "minio_s3_object" "autoyast_amd64" {
   bucket_name    = "matchbox-assets"
-  object_name    = "kubix86/autoyast2/kubicamd64.xml"
+  object_name    = "kubic86/autoyast2/kubicamd64.xml"
   content        = file("${path.module}/autoyast-amd64.xml")
 
 }
