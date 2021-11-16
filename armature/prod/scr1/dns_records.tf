138a67f1f85bd3ffe325c6267c02af96357a1360
Anthony Rabbito <hello@anthonyrabbito.com>
2021-11-16 18:59:31 -0500

    feat!: new dns!
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>
    
    fix: readme
    
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- armature/prod/scr1/dns_records.tf
+++ armature/prod/scr1/dns_records.tf
@@ -6,7 +6,7 @@
 resource "cloudflare_record" "cluster_0_ie" {
   name    = "cluster-0-ie.scr1.rabbito.tech"
   zone_id = data.sops_file.cloudflare_secrets.data["int_zone_id"]
-  value   = "73.101.222.32"
+  value   = "50.223.56.162"
   proxied = false
   type    = "A"
 }
