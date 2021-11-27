9465277f95800f74f573a64dbeb86cdec4d670be
Anthony Rabbito <hello@anthonyrabbito.com>
2021-11-27 15:18:58 -0500

    fix: remove zzh mount
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>
    
    fix: drop nfd
    
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- armature/prod/scr1/dns_records.tf
+++ armature/prod/scr1/dns_records.tf
@@ -10,3 +10,18 @@ resource "cloudflare_record" "cluster_0_ie" {
   proxied = false
   type    = "A"
 }
+
+# reverse dns
+resource "cloudflare_record" "rdns_3g" {
+  zone_id = data.sops_file.cloudflare_secrets.data["int_zone_id"]
+  name    = "162.56.223.50.in-addr.arpa"
+  type    = "PTR"
+  value   = "scr1.rabbito.tech"
+}
+
+resource "cloudflare_record" "rdns_1g" {
+  zone_id = data.sops_file.cloudflare_secrets.data["int_zone_id"]
+  name    = "50.239.94.170.in-addr.arpa"
+  type    = "PTR"
+  value   = "scr1-alt.rabbito.tech"
+}
