c0442d409ba2eaeb7f767a74a027869e97c1cb26
Anthony Rabbito <hello@anthonyrabbito.com>
2022-03-27 09:44:32 -0400

    fix: scope more perms
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- armature/prod/gcp/bootstrap/org.tf
+++ armature/prod/gcp/bootstrap/org.tf
@@ -27,13 +27,28 @@ module "bootstrap" {
   source  = "terraform-google-modules/bootstrap/google"
   version = "5.0.0"
 
-  org_id               = var.org_id
-  billing_account      = var.billing_account
-  group_org_admins     = var.group_org_admins
-  group_billing_admins = var.group_billing_admins
-  default_region       = "us-east4"
-  project_id           = "tf-seed"
-  folder_id            = google_folder.bootstrap.id
+  org_id                  = var.org_id
+  billing_account         = var.billing_account
+  group_org_admins        = var.group_org_admins
+  group_billing_admins    = var.group_billing_admins
+  sa_enable_impersonation = true
+  default_region          = "us-east4"
+  project_id              = "tf-seed"
+  folder_id               = google_folder.bootstrap.id
+  sa_org_iam_permissions = [
+    "roles/accesscontextmanager.policyAdmin",
+    "roles/billing.user",
+    "roles/compute.networkAdmin",
+    "roles/compute.xpnAdmin",
+    "roles/iam.securityAdmin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/logging.configWriter",
+    "roles/orgpolicy.policyAdmin",
+    "roles/resourcemanager.projectCreator",
+    "roles/resourcemanager.folderAdmin",
+    "roles/securitycenter.notificationConfigEditor",
+    "roles/resourcemanager.organizationViewer"
+  ]
 }
 
 module "gh_oidc" {
