c8a6c1a0f8b66bee27ca34b06f31eff0a65b9e3b
Anthony Rabbito <hello@anthonyrabbito.com>
2022-11-12 14:54:21 -0500

    feat(wip): worker-13 on fcos
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

---
+++ Justfile
@@ -0,0 +1,4 @@
+build-push-k8s-node:
+  ./armature/prod/fcos-layers/k8s-node/build-push.sh
+
+
---
+++ armature/prod/fcos-layers/k8s-node/build-push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$HOSTNAME" == "toolbox" ]
+then
+  BUILDAH="host-spawn buildah"
+  PODMAN="host-spawn podman"
+else
+  BUILDAH="BUILDAH"
+  PODMAN="PODMAN"
+fi
+
+PLATFORM=linux/amd64,linux/arm64
+$BUILDAH build --jobs=2 --platform=$PLATFORM --manifest fcos-k8s-node .
+$BUILDAH tag localhost/fcos-k8s-node ghcr.io/anthr76/fcos-k8s-node:stable
+$PODMAN manifest rm localhost/fcos-k8s-node
+$PODMAN manifest push --all ghcr.io/anthr76/fcos-k8s-node:stable docker://ghcr.io/anthr76/fcos-k8s-node:stable
