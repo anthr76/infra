1c681924d453612c70c6a1d220a0a3a95938255f
Anthony Rabbito <hello@anthonyrabbito.com>
2022-12-04 21:58:30 -0500

    feat!: remove ansible
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- ansible/roles/bootstrap_os/README.md
+++
@@ -1,1 +0,0 @@
-We don't use a ansible os variable to ensure python is present.
--- ansible/roles/vyos/files/postconfig-bootup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# This script is executed at boot time after VyOS configuration is fully applied.
-# Any modifications required to work around unfixed bugs
-# or use services not available through the VyOS CLI system can be placed here.
