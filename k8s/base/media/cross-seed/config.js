002013ae0e929b0e5cce3bd51cf9ed0d55867f2d
Anthony Rabbito <hello@anthonyrabbito.com>
2024-01-13 16:39:47 -0500

    fix: ports
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- k8s/base/media/cross-seed/config.js
+++ k8s/base/media/cross-seed/config.js
@@ -3,16 +3,16 @@ module.exports = {
   delay: 20,
   qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local:8080",
   torznab: [
-    `http://prowlarr.default.svc.cluster.local:8080/4/api?apikey=${process.env.PROWLARR_API_KEY}`, // at
-    `http://prowlarr.default.svc.cluster.local:8080/16/api?apikey=${process.env.PROWLARR_API_KEY}`, // ant
-    `http://prowlarr.default.svc.cluster.local:8080/2/api?apikey=${process.env.PROWLARR_API_KEY}`, // blu
-    `http://prowlarr.default.svc.cluster.local:8080/1/api?apikey=${process.env.PROWLARR_API_KEY}`, // btn
-    `http://prowlarr.default.svc.cluster.local:8080/12/api?apikey=${process.env.PROWLARR_API_KEY}`, // cz
-    `http://prowlarr.default.svc.cluster.local:8080/3/api?apikey=${process.env.PROWLARR_API_KEY}`, // hdt
-    `http://prowlarr.default.svc.cluster.local:8080/15/api?apikey=${process.env.PROWLARR_API_KEY}`, // ipt
-    `http://prowlarr.default.svc.cluster.local:8080/5/api?apikey=${process.env.PROWLARR_API_KEY}`, // ms
-    `http://prowlarr.default.svc.cluster.local:8080/14/api?apikey=${process.env.PROWLARR_API_KEY}`, // tl
-    `http://prowlarr.default.svc.cluster.local:8080/13/api?apikey=${process.env.PROWLARR_API_KEY}`, // ts
+    `http://prowlarr.default.svc.cluster.local:9696/4/api?apikey=${process.env.PROWLARR_API_KEY}`, // at
+    `http://prowlarr.default.svc.cluster.local:9696/16/api?apikey=${process.env.PROWLARR_API_KEY}`, // ant
+    `http://prowlarr.default.svc.cluster.local:9696/2/api?apikey=${process.env.PROWLARR_API_KEY}`, // blu
+    `http://prowlarr.default.svc.cluster.local:9696/1/api?apikey=${process.env.PROWLARR_API_KEY}`, // btn
+    `http://prowlarr.default.svc.cluster.local:9696/12/api?apikey=${process.env.PROWLARR_API_KEY}`, // cz
+    `http://prowlarr.default.svc.cluster.local:9696/3/api?apikey=${process.env.PROWLARR_API_KEY}`, // hdt
+    `http://prowlarr.default.svc.cluster.local:9696/15/api?apikey=${process.env.PROWLARR_API_KEY}`, // ipt
+    `http://prowlarr.default.svc.cluster.local:9696/5/api?apikey=${process.env.PROWLARR_API_KEY}`, // ms
+    `http://prowlarr.default.svc.cluster.local:9696/14/api?apikey=${process.env.PROWLARR_API_KEY}`, // tl
+    `http://prowlarr.default.svc.cluster.local:9696/13/api?apikey=${process.env.PROWLARR_API_KEY}`, // ts
   ],
   port: process.env.CROSSSEED_PORT || 80,
   apiAuth: false,
