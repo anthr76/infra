e051a4a42601244ef8145d0281d8b28dd3351b8c
Anthony Rabbito <hello@anthonyrabbito.com>
2024-04-28 10:45:50 -0400

    hack: lie about scr1 unti it's qgr
    Signed-off-by: Anthony Rabbito <hello@anthonyrabbito.com>

--- armature/prod/scr1/dns_records.tf
+++ armature/prod/scr1/dns_records.tf
@@ -6,22 +6,7 @@
 resource "cloudflare_record" "cluster_0_ie" {
   name    = "cluster-0-ie.scr1.rabbito.tech"
   zone_id = data.sops_file.cloudflare_secrets.data["int_zone_id"]
-  value   = "50.223.56.162"
+  value   = "24.229.169.35"
   proxied = false
   type    = "A"
-}
-
-# reverse dns
-resource "cloudflare_record" "rdns_3g" {
-  zone_id = data.sops_file.cloudflare_secrets.data["int_zone_id"]
-  name    = "162.56.223.50.in-addr.arpa"
-  type    = "PTR"
-  value   = "scr1.rabbito.tech"
-}
-
-resource "cloudflare_record" "rdns_1g" {
-  zone_id = data.sops_file.cloudflare_secrets.data["int_zone_id"]
-  name    = "50.239.94.170.in-addr.arpa"
-  type    = "PTR"
-  value   = "scr1-alt.rabbito.tech"
 }
